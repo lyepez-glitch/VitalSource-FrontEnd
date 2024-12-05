1d48c91bef3f8520be07af9d3d5e783cde968941
Lucas Yepez <lucasyepez99@gmail.com>
2024-12-05 15:32:37 -0600

    edited url

--- vitalcore/src/app/page.tsx
+++ vitalcore/src/app/page.tsx
@@ -10,7 +10,7 @@ import {io} from "socket.io-client"; // Import socket.io-client
 
 
 
-const socket = io("http://localhost:8080");
+const socket = io("https://vitalcore.onrender.com");
 // Define a type for genes
 interface Gene {
   id: string;
