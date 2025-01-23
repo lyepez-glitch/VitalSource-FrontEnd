4c11a28278e02bf24490e4b28a401cbcb01a5ef8
Lucas Yepez <lucasyepez99@gmail.com>
2025-01-22 23:07:07 -0600

    testing linter

--- vitalcore/src/app/page.tsx
+++ vitalcore/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Chart from "./components/Chart";
 import Dashboard from "./components/Dashboard";
 import PopulationEffectsChart from "./components/PopulationEffectsChart";
