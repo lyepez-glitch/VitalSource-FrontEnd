9703a478f5feb9ccde30c011b826ab83bc9e6ddb
Lucas Yepez <lucasyepez99@gmail.com>
2025-06-12 14:37:54 -0500

    debugging vercel issue

--- vitalcore/src/app/components/Simulate.tsx
+++ vitalcore/src/app/components/Simulate.tsx
@@ -1,4 +1,4 @@
-// import React, { useState,useEffect } from 'react';
+/// import React, { useState,useEffect } from 'react';
 import Chart from "./Chart";
 import Dashboard from "./Dashboard";
 import PopulationEffectsChart from "./PopulationEffectsChart";
